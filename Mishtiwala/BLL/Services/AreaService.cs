fcc9a788f49b4fba5a7346b8f53275600812fa10
Rafidur <19-39956-1@student.aiub.edu>
2023-05-05 00:04:18 +0600

    area crud done

---
+++ Mishtiwala/BLL/Services/AreaService.cs
@@ -0,0 +1,77 @@
+﻿using BLL.DTOs;
+using DAL.Models;
+using DAL.Repos;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class AreaService
+    {
+        private readonly IRepository<Area> _areaRepository;
+        public AreaService()
+        {
+            _areaRepository = DataAccessFactory.AreaData();
+        }
+
+        public void CreateArea(AreaDTO areaDTO)
+        {
+            _areaRepository.Add(new Area
+            {
+                Name = areaDTO.Name,
+            });
+        }
+
+        public AreaDTO GetAreaById(int id)
+        {
+            var areaEntity = _areaRepository.GetById(id);
+            if (areaEntity == null)
+            {
+                return null;
+            }
+            AreaDTO area = new AreaDTO
+            {
+                Name = areaEntity.Name,
+                Id = areaEntity.Id
+            };
+            return area;
+        }
+
+        public List<AreaDTO> GetAllAreas()
+        {
+            var list = _areaRepository.GetAll();
+            var areas = new List<AreaDTO>();
+            foreach (var area in list)
+            {
+                areas.Add(new AreaDTO
+                {
+                    Name = area.Name,
+                    Id = area.Id
+                });
+            }
+            return areas;
+        }
+
+        public void UpdateArea(AreaDTO area, int id)
+        {
+            var entity = _areaRepository.GetById(id);
+            if (entity == null)
+            {
+                return;
+            }
+            entity.Name = area.Name;
+            entity.Id = area.Id;
+            _areaRepository.Update(entity);
+        }
+
+        public void DeleteArea(int id)
+        {
+            var entity = _areaRepository.GetById(id);
+            _areaRepository.Delete(entity);
+        }
+    }
+}
---
+++ Mishtiwala/Mishtiwala/Controllers/AreaController.cs
@@ -0,0 +1,76 @@
+﻿using BLL.DTOs;
+using BLL.Services;
+using Mishtiwala.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Mishtiwala.Controllers
+{
+    public class AreaController : ApiController
+    {
+        private readonly AreaService _areaService;
+
+        public AreaController()
+        {
+            _areaService = new AreaService();
+        }
+
+        [HttpGet]
+        [Route("api/area")]
+        public HttpResponseMessage Get()
+        {
+            var areaList = _areaService.GetAllAreas();
+            return Request.CreateResponse(HttpStatusCode.OK, areaList);
+        }
+
+        [HttpGet]
+        [Route("api/area/{id}")]
+        public HttpResponseMessage Get(int id)
+        {
+            var area = _areaService.GetAreaById(id);
+            return Request.CreateResponse(HttpStatusCode.OK, area);
+        }
+
+        [HttpPost]
+        [Route("api/area/add")]
+        public HttpResponseMessage Add(AreaCreateModel area)
+        {
+            if (ModelState.IsValid)
+            {
+                AreaDTO areaDTO = new AreaDTO
+                {
+                    Name = area.Name,
+                };
+                try
+                {
+                    _areaService.CreateArea(areaDTO);
+                    return Request.CreateResponse(HttpStatusCode.OK, "Area Created");
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+                }
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+        [HttpPatch]
+        [Route("api/area/update/{id}")]
+        public HttpResponseMessage Patch(AreaDTO area, int id)
+        {
+            _areaService.UpdateArea(area, id);
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        [HttpDelete]
+        [Route("api/area/delete/{id}")]
+        public HttpResponseMessage Delete(int id)
+        {
+            _areaService.DeleteArea(id);
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
